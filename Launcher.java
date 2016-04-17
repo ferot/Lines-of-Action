16ce3bd6afba4abcc64c41a948b037d4c3b50042
elkapitano20 <elkapitano40@gmail.com>
2016-04-17 13:38:01 +0200

    Added files via upload

--- Launcher.java
+++ Launcher.java
@@ -8,8 +8,8 @@ import board.Board;
 
 public class Launcher extends JFrame {
 
-	private static final int WIDTH_FIX = 16;
-	private static final int HEIGHT_FIX = 38;
+	private static final int WIDTH_FIX = 5;
+	private static final int HEIGHT_FIX = 27;
 	private final static String GAME_NAME = "Lines of Action";
 
 	public Launcher() {
@@ -25,6 +25,7 @@ public class Launcher extends JFrame {
 				.getWidth(null) + WIDTH_FIX), (int) (Szachownica.getPlansza()
 				.getImage().getHeight(null) + HEIGHT_FIX));
 		launch.add(new Board());
+		launch.setResizable(false);
 		launch.repaint();
 	}
 
